596b3b56b94ed8074bebfda455f8dcc0df4e4a6a
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-01-05 12:19:07 +0700

    sample fetch

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.bitbucket.org/2.0/user", nil)
+	if err != nil {
+		panic(err)
+	}
+	bbUser := os.Getenv("BB_USER")
+	bbPass := os.Getenv("BB_PASS")
+	req.SetBasicAuth(bbUser, bbPass)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp)
 }
