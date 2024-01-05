f45040317d7df084757531d11dfcae5410686be7
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-01-05 12:26:52 +0700

    use .env

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Response struct {
 	DisplayName string `json:"display_name"`
+	Username    string `json:"username"`
 }
 
 func main() {
+	godotenv.Load()
+
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://api.bitbucket.org/2.0/user", nil)
 	if err != nil {
@@ -35,4 +40,5 @@ func main() {
 	var response Response
 	json.Unmarshal(body, &response)
 	fmt.Println("Name:", response.DisplayName)
+	fmt.Println("Username:", response.Username)
 }
